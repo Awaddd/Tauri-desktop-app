bf6f3c569c824abf14d6c615cce22e3bbd8f9f86
Awad <34748292+Awaddd@users.noreply.github.com>
2023-08-19 07:56:11 +0100

    Clean up database connection

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -2,19 +2,15 @@ use std::env;
 use std::error::Error;
 
 pub async fn start_db() -> Result<sqlx::PgPool, Box<dyn Error>> {
-  let user = env::var("DB_USER");
+  let user = env::var("DB_USER")?;
+  let password = env::var("DB_PASSWORD")?;
+  let host = env::var("DB_HOST")?;
+  let name = env::var("DB_NAME")?;
 
-  match env::var("DB_USER") {
-    Ok(v) => println!("{}: {}", "DB_USER", v),
-    Err(e) => println!("${} is not set ({})", "DB_USER", e)
-  }
+  let url = format!("postgres://{:}:{:}@{:}/{:}", &user, &password, &host, &name);
 
-  let password = env::var("DB_PASSWORD");
-  let host = env::var("DB_HOST");
-  let name = env::var("DB_NAME");
+  println!("url {}", &url);
 
-  let url = format!("postgres://{:?}:{:?}@{:?}/{:?}", &user, &password, &host, &name);
-  println!("url {}", url);
   let pool = sqlx::postgres::PgPool::connect(&url).await?;
 
   sqlx::migrate!("./migrations").run(&pool).await?;
